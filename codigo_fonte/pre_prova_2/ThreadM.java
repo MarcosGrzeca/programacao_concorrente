124221e0cdb1ac5a5209f4ede1f3e3bba714a9ee
MarcosGrzeca <marcos-a-g@hotmail.com>
2015-12-07 10:47:53 -0200

    exercicios

---
+++ codigo_fonte/pre_prova_2/ThreadM.java
@@ -0,0 +1,10 @@
+public class ThreadM extends Thread {
+
+	public ThreadM() {
+
+	}
+
+	public void run() {
+		System.out.println("MAROCS");
+	}
+}
---
+++ codigo_fonte/pre_prova_2/exe1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+
+struct args {
+	int id;
+	int termos;
+	int threads;
+	int resultado;
+};
+
+
+typedef struct args ARGS;
+
+void *funcao (void * args) {
+	ARGS *p = (ARGS *) args;
+
+	int id = p->id;
+	int termos = p->termos;
+	int threads = p->threads;
+	int i;
+	double resultado;
+
+	for (i = id; i <= termos; i+= threads) {
+		if (i % 2) {
+			resultado += 1/(i * 2 -1);
+		} else {
+			resultado -= 1/(i * 2 -1);
+		}
+	}
+
+	p->resultado = resultado;
+}
+
+void main() {
+
+	pthread_t id[3];
+	int i, termos = 10, threads = 3;
+	ARGS *argumentos = NULL;
+	double total;
+
+	argumentos = (ARGS *)malloc(threads * sizeof(ARGS));
+
+	for (i = 0; i < threads; i++) {
+		argumentos[i]->id = i + 1; 
+		argumentos[i]->termos = termos;
+		argumentos[i]->threads = threads;
+		pthread_create(&id[i], NULL, funcao, (void *) argumentos[i]);
+	}
+
+	for (i = 0; i < threads; i++) {
+		pthread_join(id[i], NULL);
+		total += argumentos[i]->resultado;
+	}
+
+	printf("%.15f\n", 4 * total);
+
+
+
+}
