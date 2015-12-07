6327aea2f4bccbedf0b5101a65e85060cb54ff47
Ubuntu <marcos-a-g@hotmail.com>
2015-12-07 11:07:43 -0200

    exercicio

--- codigo_fonte/pre_prova_2/exe1.c
+++ codigo_fonte/pre_prova_2/exe1.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <math.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 struct args {
 	int id;
-	int termos;
 	int threads;
-	int resultado;
+	int termos;
+	double resultado;
 };
-
-
 typedef struct args ARGS;
 
 void *funcao (void * args) {
@@ -19,41 +18,42 @@ void *funcao (void * args) {
 	int termos = p->termos;
 	int threads = p->threads;
 	int i;
-	double resultado;
+	double parcial = 0;
 
 	for (i = id; i <= termos; i+= threads) {
 		if (i % 2) {
-			resultado += 1/(i * 2 -1);
+			parcial += 1.0/(i * 2 -1);
 		} else {
-			resultado -= 1/(i * 2 -1);
+			parcial -= 1.0/(i * 2 -1);
 		}
 	}
-
-	p->resultado = resultado;
+	p->resultado = parcial;
 }
 
 void main() {
 
-	pthread_t id[3];
-	int i, termos = 10, threads = 3;
+	pthread_t *id = NULL;
+	int i, termos = 40, threads = 5;
 	ARGS *argumentos = NULL;
-	double total;
+	double total = 0;
 
 	argumentos = (ARGS *)malloc(threads * sizeof(ARGS));
+	id = (pthread_t *)malloc(threads * sizeof(pthread_t));
 
 	for (i = 0; i < threads; i++) {
-		argumentos[i]->id = i + 1; 
-		argumentos[i]->termos = termos;
-		argumentos[i]->threads = threads;
+		argumentos[i].id = i + 1; 
+		argumentos[i].termos = termos;
+		argumentos[i].threads = threads;
+		argumentos[i].resultado = 0;
 		pthread_create(&id[i], NULL, funcao, (void *) &argumentos[i]);
 	}
 
 	for (i = 0; i < threads; i++) {
 		pthread_join(id[i],NULL);
-		total += argumentos[i]->resultado;
+		total += argumentos[i].resultado;
 	}
 
-	printf("%.15f\n", 4 * total);
+	printf("PI= %.15f\n", 4 * total);
 
 
 
